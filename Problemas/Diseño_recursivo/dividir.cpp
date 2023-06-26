6fa500b6875ab22a7c8320ab336691a00fdc5a6d
antoniocenlaces <antoniocenlaces@gmail.com>
2023-06-26 20:20:26 +0000

    mas AR resueltos

--- Problemas/Diseño_recursivo/dividir.cpp
+++ Problemas/Diseño_recursivo/dividir.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// Problema AR1.4
 // Pre: a >= 0 ∧ b > 0
 // Post: cociente = a / b ∧ resto = a % b
 void dividir(const int a, const int b, int& cociente , int& resto ) {
@@ -16,6 +17,40 @@ void dividir(const int a, const int b, int& cociente , int& resto ) {
     } else {
         dividir(a - b, b, cociente, resto);
         cociente = cociente +1;
+    }
+}
+
+// Problema AR1.5
+//Pre: b >= 0
+//Post: multiplicar(a,b)=a*b
+int multiplicar(const int a, const int b) {
+    if (b == 0) {
+        return 0;
+    } else {
+        if (b % 2 == 1){
+            return a + multiplicar(2 * a, b /2);
+        } else {
+            return multiplicar(2 * a, b / 2);
+        }
+    }
+}
+
+// Problema AR1.6
+// Pre: n >= 0 AND d >= ’0’ AND d <= ’9’
+//  Post: Devuelve el número resultante de eliminar de <n> todos los
+//        dígitos iguales a <d>.
+int eliminar (const int n, const int d) {
+    if ( n <= 9) {
+        if (n == d) {
+            return 0;
+        } else {
+            return n;
+        }
+    }
+    if (n % 10 == d) {
+        return eliminar(n / 10, d);
+    } else {
+        return n % 10 + eliminar(n / 10, d) * 10;
     }
 }
 
@@ -33,5 +68,15 @@ int main() {
 
     cout << "Operadores de c++:" << endl;
     cout << a << "/" << b << "= " << a / b << endl;
-    cout << a << "%" << b << "= " << a % b << endl;
+    cout << a << "%" << b << "= " << a % b << endl << endl;
+
+    cout << "Multiplicación recursiva:" << endl;
+    cout << a << "*" << b << "= " << multiplicar(a, b) << endl << endl;
+    cout << "Por compliador: " << a * b << endl;
+
+    cout << endl << "Prueba de eliminar" << endl;
+    int elimina;
+    cout << "Dígito a eliminar de " << a << endl;
+    cin >> elimina;
+    cout << "Eliminando " << elimina << " queda: " << eliminar(a,elimina) << endl;
 }
