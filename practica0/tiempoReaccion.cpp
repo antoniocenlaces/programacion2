4bebd51c1c20041d3e3b62e039f8d0bb4f40c696
antoniocenlaces <antoniocenlaces@gmail.com>
2023-12-04 20:34:24 +0000

    ajustado tiempoReaccion

--- practica0/tiempoReaccion.cpp
+++ practica0/tiempoReaccion.cpp
@@ -5,18 +5,24 @@
 using namespace std;
 
 int main(int numArg, char* v[]) {
-    time_t t1,t2;
-    int n = atoi(v[1]);
-    for (unsigned i=0; i < n; i++) {
-        cout << i + 1 << ") Pulse la tecla de fin de línea, por favor ...";
-        t1 = time(nullptr);
-       
-           cin.get();
+   if (numArg == 2){ 
+        time_t t1,t2;
+        int n = atoi(v[1]);
+        for (unsigned i=0; i < n; i++) {
+            cout << i + 1 << ") Pulse la tecla de fin de línea, por favor ...";
+            t1 = time(nullptr);
         
-        t2 = time(nullptr);
-        int segundos = int(t2 - t1);
-        cout << "Su tiempo de reacción ha sido de " << segundos << " segundos" << endl;
-        cout << endl;
+            cin.get();
+            
+            t2 = time(nullptr);
+            int segundos = int(t2 - t1);
+            cout << "Su tiempo de reacción ha sido de " << segundos << " segundos" << endl;
+            cout << endl;
+        }
+        return 0;
+    } else {
+        cout << "Formato invocación: tiempo <número de repeticiones>\n";
+        return 1;
     }
-    return 0;
+    
 }
